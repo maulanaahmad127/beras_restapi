7137e4453bab766f3039dc1d5da5aac5039fc38b
Maulana Ahmmad Rodhi Alfian <maulahmaddd40@gmail.com>
2023-08-30 13:30:05 +0700

    new change username data dto

---
+++ src/main/java/com/bezkoder/spring/entity/dto/ChangeUsernameData.java
@@ -0,0 +1,11 @@
+package com.bezkoder.spring.entity.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class ChangeUsernameData {
+    
+    private String username;
+}

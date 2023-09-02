8abc48aecd6ce612ac9a6bcd3e3aa43b4ad05db6
Maulana Ahmmad Rodhi Alfian <maulahmaddd40@gmail.com>
2023-09-02 11:11:44 +0700

    fixing exist by id jenis beras

--- src/main/java/com/bezkoder/spring/entity/repo/JenisBerasRepo.java
+++ src/main/java/com/bezkoder/spring/entity/repo/JenisBerasRepo.java
@@ -8,5 +8,6 @@ import org.springframework.data.repository.CrudRepository;
 import com.bezkoder.spring.entity.model.JenisBeras;
 
 public interface JenisBerasRepo extends CrudRepository<JenisBeras, Long>  {
+    boolean existsById(Long id);
 }
     
